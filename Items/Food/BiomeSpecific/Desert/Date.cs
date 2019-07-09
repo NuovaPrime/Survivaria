084ed32166f7cb9a1685e7e199cbac862569cdb9
NuovaPrime <loganman51@hotmail.com>
2019-07-09 17:17:20 -0600

    Transferring over my old work

---
+++ Items/Food/BiomeSpecific/Desert/Date.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Survivaria.Items.Food.BiomeSpecific.Desert
+{
+    public class Date : FoodItem
+    {
+        public Date() : base("Date", "A nutritious food for expeditions in the desert.", 16, 24, 4, SoundID.Item1, 999)
+        {
+        }
+    }
+}
