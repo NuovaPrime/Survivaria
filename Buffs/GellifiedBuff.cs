1dfbd88d6c8ae82fd955adf85121ab8c3a40a36b
NuovaPrime <loganman51@hotmail.com>
2019-08-07 03:49:59 -0600

    Sprite uploads

---
+++ Buffs/GellifiedBuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Survivaria.Buffs
+{
+    public class GellifiedBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Gellified");
+            Description.SetDefault("Increased jump height and immunity to slimes. Reduced movement speed.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+        internal int[] SlimeIDs = new int[] { NPCID.BlueSlime, NPCID.GreenSlime, NPCID.PurpleSlime, NPCID.RedSlime, NPCID.SandSlime, NPCID.Slimeling, NPCID.Slimer, NPCID.Slimer2, NPCID.YellowSlime, NPCID.BlackSlime, NPCID.BabySlime, NPCID.BunnySlimed, NPCID.BunnySlimed, NPCID.CorruptSlime, NPCID.DungeonSlime, NPCID.RainbowSlime, NPCID.SlimeSpiked, NPCID.SpikedIceSlime, NPCID.SpikedJungleSlime, NPCID.UmbrellaSlime, NPCID.SlimeRibbonGreen, NPCID.SlimeRibbonRed, NPCID.SlimeRibbonWhite, NPCID.SlimeRibbonYellow };
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.jumpSpeedBoost += 0.2f;
+            foreach (var ID in SlimeIDs)
+            {
+                player.npcTypeNoAggro[ID] = true;
+            }
+            player.moveSpeed -= 0.10f;
+        }
+    }
+}
--- Items/BossItems/TubofSlime.cs
+++ Items/BossItems/TubofSlime.cs
@@ -8,7 +8,7 @@ namespace Survivaria.Items.BossItems
 {
     public class TubofSlime : SurvivariaItem //Making this a survivariaitem because boss items are too unique and should not be influenced by any buffs or debuffs inflicted upon all foods.
     {
-        public TubofSlime() : base("Tub of Slime", "A tub of slime that seems to refill itself instantly when consumed, it has a very unique smell and texture to it. \nGives a boost to jump height and immunity to slimes for 3 minutes but also reduces movement speed. \nUnique food", 28, 26, Item.buyPrice(0, 1, 0, 0), ItemRarityID.Green, 70, 0, SoundID.Item28, 0 /*Survivaria.Instance.BuffType("GellifiedBuff")*/, 10800, 1)
+        public TubofSlime() : base("Tub of Slime", "A tub of slime that seems to refill itself instantly when consumed, it has a very unique smell and texture to it. \nGives a boost to jump height and immunity to slimes for 3 minutes but also reduces movement speed. \nUnique food", 28, 26, Item.buyPrice(0, 1, 0, 0), ItemRarityID.Green, 70, 0, SoundID.Item28, Survivaria.Instance.BuffType("GellifiedBuff"), 10800, 1)
         {
         }
 
