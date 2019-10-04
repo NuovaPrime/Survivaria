42b790f4eb6d440fc0f0d7c1c7f9d6fbfda3dfef
Aurian <theorian34@gmail.com>
2019-10-04 16:51:47 +0200

    added effect

--- Items/Food/BiomeSpecific/Crimson/Greneyede.cs
+++ Items/Food/BiomeSpecific/Crimson/Greneyede.cs
@@ -8,7 +8,7 @@ namespace Survivaria.Items.Food.BiomeSpecific.Crimson
 {
     public class Greneyede : FoodItem
     {
-        public Greneyede() : base("Greneyede", "Tastes surprisingly good, but you don't feel safe being watched when eating it.", 16, 24, Item.buyPrice(0, 0, 0, 80), ItemRarityID.Blue, 5, 3, SoundID.Item2)
+        public Greneyede() : base("Greneyede", "Tastes surprisingly good, but you don't feel safe being watched when eating it.\nIncreases spawn rate for 30 seconds.", 16, 24, Item.buyPrice(0, 0, 0, 80), ItemRarityID.Blue, 5, 3, SoundID.Item2, BuffID.Battle, 30 * 60)
         {
         }
     }
