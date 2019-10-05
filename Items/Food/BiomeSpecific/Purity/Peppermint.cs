db915c7bf9332fd122631bb1d8f7f6130cd2eb95
Aurian <theorian34@gmail.com>
2019-10-05 10:34:28 +0200

    add buff

--- Items/Food/BiomeSpecific/Purity/Peppermint.cs
+++ Items/Food/BiomeSpecific/Purity/Peppermint.cs
@@ -8,7 +8,7 @@ namespace Survivaria.Items.Food.BiomeSpecific.Purity
 {
     public class Peppermint : FoodItem
     {
-        public Peppermint() : base("Peppermint", "Being too close already makes eyes water and noses run. Caution is advised.", 24, 30, Item.buyPrice(0, 0, 1, 0), ItemRarityID.Blue, 2, -5, SoundID.Item2)
+        public Peppermint() : base("Peppermint", "Being too close already makes eyes water and noses run. Caution is advised.\nGrants panic for 30 seconds.", 24, 30, Item.buyPrice(0, 0, 1, 0), ItemRarityID.Blue, 2, -5, SoundID.Item2, BuffID.Panic, 60 * 30)
         {
         }
     }
