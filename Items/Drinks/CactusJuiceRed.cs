c57955c0841b48b62464561d83b7eba60a9ef68c
Aurian <theorian34@gmail.com>
2019-10-13 18:12:14 +0200

    added cactus juice variants

---
+++ Items/Drinks/CactusJuiceRed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Survivaria.Items.Drinks
+{
+    public class CactusJuiceRed : DrinkItem
+    {
+        public CactusJuiceRed() : base("Cactus Juice", "Some concentrated and refreshing cactus juice.", 20, 26, Item.buyPrice(0, 0, 2, 5), ItemRarityID.Blue, 2, 14, BuffID.Thorns, 60 * 90)
+        {
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<PricklyPearRed>());
+            recipe.AddIngredient(ItemID.Cactus);
+            recipe.AddIngredient(ItemID.Bottle);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
