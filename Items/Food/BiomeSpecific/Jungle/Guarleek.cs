bd68e9c2fa24dc9e48719be8e0383df6423fe308
Aurian <theorian34@gmail.com>
2019-10-14 12:35:13 +0200

    some changes

--- Items/Food/BiomeSpecific/Jungle/Guarleek.cs
+++ Items/Food/BiomeSpecific/Jungle/Guarleek.cs
@@ -8,8 +8,15 @@ namespace Survivaria.Items.Food.BiomeSpecific.Jungle
 {
     public class Guarleek : FoodItem
     {
-        public Guarleek() : base("Guarleek", "It is said that the smell isn't for the faint-hearted, but those are just rumors.\nIncreases max life for 60 seconds.", 24, 30, Item.buyPrice(0, 0, 1, 0), ItemRarityID.Blue, 5, 1, SoundID.Item2, BuffID.Lifeforce, 60 * 60)
+        public Guarleek() : base("Guarleek", "It is said that the smell isn't for the faint-hearted, but those are just rumors.", 24, 30, Item.buyPrice(0, 2, 0, 0), ItemRarityID.Lime, 5, 1, SoundID.Item2, BuffID.Lifeforce, 60 * 60)
         {
         }
+
+		public override bool UseItem(Player player)
+        {
+            player.AddBuff(BuffID.Stinky, 60 * 60);
+            return true;
+        }
+
     }
 }
