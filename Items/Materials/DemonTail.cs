bdbdf757f633f46a682c2693ada88b317d3ff183
Aurian <theorian34@gmail.com>
2019-12-13 18:00:38 +0100

    some tweaks

--- Items/Materials/DemonTail.cs
+++ Items/Materials/DemonTail.cs
@@ -11,7 +11,7 @@ namespace Survivaria.Items.Materials
 {
     public class DemonTail : SurvivariaItem
     {
-        public DemonTail() : base("Demon Tail", "Many would be surprised that it can even be eaten.", 20, 30, Item.buyPrice(0, 0, 50, 0), ItemRarityID.Blue, 0, 0)
+        public DemonTail() : base("Demon Tail", "Many would be surprised that it can even be eaten.", 20, 30, Item.buyPrice(0, 3, 0, 0), ItemRarityID.LightPurple, 0, 0)
         {
         }
     }
